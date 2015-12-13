5be7d16212737f04cee5cb4b8defb62a939cf030
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:22:36 -0600

    Impuestos

---
+++ Impuestos/Impuestos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impuestos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sueldo;
+            string linea;
+            Console.Write("Ingrese el sueldo:");
+            linea = Console.ReadLine();
+            sueldo = float.Parse(linea);
+            if (sueldo > 3000)
+            {
+                Console.Write("Esta persona debe abonar impuestos");
+            }
+            if (sueldo < 3000 && sueldo >0)
+            {
+                Console.Write("Esta persona no debe abonar impuestos");
+            }
+            if (sueldo <= 0)
+            {
+                Console.Write("Esta cantidad no existe");
+            }
+            Console.ReadKey();
+        }
+    }
+}
