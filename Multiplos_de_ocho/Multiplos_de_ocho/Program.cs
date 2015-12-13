6d8e899d370781a6d8826cdc671c61385135d788
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:25:26 -0600

    Multiplos de Ocho

---
+++ Multiplos_de_ocho/Multiplos_de_ocho/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos_de_ocho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero=8;
+            int NumeroFinal = 500;
+
+            while(Numero<=NumeroFinal){
+                Console.Write(Numero);
+                Numero=Numero+8;
+
+                if (Numero < 500)
+                {
+                    Console.Write(" - ");
+                }
+            }
+            Console.Write(" - "+NumeroFinal);
+            Console.ReadKey();
+        }
+    }
+}
