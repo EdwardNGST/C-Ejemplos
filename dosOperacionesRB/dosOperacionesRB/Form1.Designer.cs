f2ac55b99658cf5c811234c441a8b7ec96879e33
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:17:01 -0600

    Dos Operaciones
    NPI

---
+++ dosOperacionesRB/dosOperacionesRB/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace dosOperacionesRB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RadioBoton1 = new System.Windows.Forms.RadioButton();
+            this.RadioBoton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Resultado:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // RadioBoton1
+            // 
+            this.RadioBoton1.AutoSize = true;
+            this.RadioBoton1.Location = new System.Drawing.Point(24, 82);
+            this.RadioBoton1.Name = "RadioBoton1";
+            this.RadioBoton1.Size = new System.Drawing.Size(55, 17);
+            this.RadioBoton1.TabIndex = 7;
+            this.RadioBoton1.TabStop = true;
+            this.RadioBoton1.Text = "Sumar";
+            this.RadioBoton1.UseVisualStyleBackColor = true;
+            // 
+            // RadioBoton2
+            // 
+            this.RadioBoton2.AutoSize = true;
+            this.RadioBoton2.Location = new System.Drawing.Point(24, 114);
+            this.RadioBoton2.Name = "RadioBoton2";
+            this.RadioBoton2.Size = new System.Drawing.Size(56, 17);
+            this.RadioBoton2.TabIndex = 8;
+            this.RadioBoton2.TabStop = true;
+            this.RadioBoton2.Text = "Restar";
+            this.RadioBoton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 180);
+            this.Controls.Add(this.RadioBoton2);
+            this.Controls.Add(this.RadioBoton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton RadioBoton1;
+        private System.Windows.Forms.RadioButton RadioBoton2;
+    }
+}
+
---
+++ dosOperacionesRB/dosOperacionesRB/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dosOperacionesRB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (RadioBoton1.Checked == true)
+            {
+                int valor1 = int.Parse(textBox1.Text);
+                int valor2 = int.Parse(textBox2.Text);
+                int suma = valor1 + valor2;
+                label3.Text = suma.ToString();
+            }
+            if (RadioBoton2.Checked == true)
+            {
+                int valor1 = int.Parse(textBox1.Text);
+                int valor2 = int.Parse(textBox2.Text);
+                int resta = valor1 - valor2;
+                label3.Text = resta.ToString();
+            }
+        }
+    }
+}
