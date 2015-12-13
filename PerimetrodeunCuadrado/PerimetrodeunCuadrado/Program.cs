c17fcb6a0d2f99dfbaae3c3aa0f015317b43f3fd
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 17:34:04 -0600

    Perímetro de un Cuadrado

---
+++ PerimetrodeunCuadrado/PerimetrodeunCuadrado/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetrodeunCuadrado{
+    class Program{
+        static void Main(string[] args){
+
+            String Linea;
+            float Lado;
+            float Perimetro;
+
+            Console.Write("Teclea el lado del cuadrado: ");
+            Linea=Console.ReadLine();
+            Lado = int.Parse(Linea);
+            Perimetro = Lado * 4;
+            Console.Write("El perimetro de tu cuadrado es: " + Perimetro);
+            Console.ReadKey();
+
+        }
+    }
+}
