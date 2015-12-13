2a605b58a2fa997cc4a74c7e287d5e66485f68ce
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:24:10 -0600

    Mi primer C#

---
+++ Mi_Primer_Proyecto_Csharp_ya/Mi_Primer_Proyecto_Csharp_ya/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_Primer_Proyecto_Csharp_ya{
+    class Program{
+        static void Main(string[] args){
+
+            int HorasTrabajadas;
+            float CostoHora;
+            float Sueldo;
+            string Linea;
+
+            Console.Write("Ingrese Horas trabajadas por el operario:");
+            Linea = Console.ReadLine();
+            HorasTrabajadas = int.Parse(Linea);
+            Console.Write("Ingrese el pago por hora:");
+            Linea = Console.ReadLine();
+            CostoHora = float.Parse(Linea);
+            Sueldo = HorasTrabajadas * CostoHora;
+            Console.Write("El sueldo total del operario es:");
+            Console.Write(Sueldo);
+            Console.ReadKey();
+
+        }
+    }
+}
