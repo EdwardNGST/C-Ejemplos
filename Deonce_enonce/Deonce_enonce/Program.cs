ac73cac9b3e02926e73eaac646a4aaa05e970ff2
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:13:18 -0600

    De once En Once

---
+++ Deonce_enonce/Deonce_enonce/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deonce_enonce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Contador=1;
+            int FinalContador=25;
+            int Numero = 11;
+
+            while (Contador <= FinalContador)
+            {
+                Console.Write(Numero);
+                if (Contador < 25)
+                {
+                    Console.Write(" - ");
+                }
+                Numero = Numero+11;
+                Contador++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
