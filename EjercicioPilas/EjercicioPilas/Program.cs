22e22a55c7502b5c79ba9e26fc1b374a578c6f6d
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:18:51 -0600

    Ejercicio Pilas

---
+++ EjercicioPilas/EjercicioPilas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPilas
+{
+    class Pila
+    {
+        class Nodo
+        {
+            public int info;
+            public Nodo sig;
+        }
+
+        private Nodo raiz;
+
+        public Pila()
+        {
+            raiz = null;
+        }
+
+        public void Insertar(int x)
+        {
+            Nodo Nuevo;
+            Nuevo = new Nodo();
+            Nuevo.info = x;
+
+            if (raiz == null)
+            {
+                Nuevo.sig = null;
+                raiz = Nuevo;
+            }
+            else
+            {
+                Nuevo.sig = raiz;
+                raiz = Nuevo;
+            }
+        }
+
+        public void Imprimir()
+        {
+            Nodo reco = raiz;
+            Console.Write("Ultimo elemento de la pila: ");
+            if (reco != null)
+            {
+                Console.Write(reco.info);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Pila Resultados= new Pila();
+            Resultados.Insertar(10);
+            Resultados.Imprimir();
+            Resultados.Insertar(20);
+            Resultados.Imprimir();
+            Resultados.Insertar(34);
+            Resultados.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
