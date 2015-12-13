66069a4197a2576c53b0795013883c9a9eaecc5e
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 15:59:29 -0600

    Notas de Alumnos

---
+++ Notas_de_alumnos/Notas_de_alumnos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas_de_alumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Linea;
+            float Calificaciones;
+            int Alumnos=1;
+            int Mayores=0;
+            int Menores = 0;
+            int Raros = 0;
+
+            while(Alumnos <11){
+                Console.Write("Ingresa la Calificacion Numero " + Alumnos+" (0-10)");
+                Linea = Console.ReadLine();
+                Calificaciones = float.Parse(Linea);
+                Alumnos++;
+                if(Calificaciones>=7){
+                    Mayores = Mayores + 1;
+                }
+
+                if (Calificaciones < 7)
+                {
+                    Menores = Menores + 1;
+                }
+            }
+
+            Console.WriteLine("La cantidad de aprobados fue: " + Mayores);
+            Console.WriteLine("La cantidad de reprobados fue: " + Menores);
+            Console.ReadKey();
+        }
+    }
+}
