bfb513c3fb7278fa6e5cbdf80c2a7b91d2a2d023
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-13 02:09:50 -0600

    alturaPersonas

---
+++ Alturadepersonas/Alturadepersonas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alturadepersonas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Linea;
+            int NPersonas;
+            float Altura;
+            float SumaAlturas=0;
+            float Promedio;
+            int Contador=1;
+
+            Console.Write("Cuantas personas se van a medir? ");
+            Linea = Console.ReadLine();
+            NPersonas = int.Parse(Linea);
+
+            while(Contador<=NPersonas){
+                Console.Write("Ingrese su altura ");
+                Linea = Console.ReadLine();
+                Altura = float.Parse(Linea);
+
+                SumaAlturas = SumaAlturas + Altura;
+                Contador++;
+            }
+
+            Promedio = SumaAlturas / NPersonas;
+
+            Console.WriteLine("El total de personas que ingresaron su altura fue: "+NPersonas);
+            Console.WriteLine("El promedio del total de las alturas fue: "+Promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
