a28a7cec0646f2ebd4f318bd6b6364b580aa2521
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-14 15:54:29 -0600

    conexion2

---
+++ PruebaDatos/PruebaDatos/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            conectar conn = new conectar();
+            label.Text = conn.obtenerDato();
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnActualizar1_Click(object sender, EventArgs e)
+        {
+            conectar conn = new conectar();
+            
+            if (txtDato.Text!="")
+            {
+                MessageBox.Show("Se actualizo el dato");
+                conn.actualizarDato(txtDato.Text);
+            }
+            else
+            {
+                MessageBox.Show("No se pudo actualizar el dato");
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            conectar conn = new conectar();
+            label.Text= conn.obtenerDato();
+        }
+    }
+}
---
+++ PruebaDatos/PruebaDatos/conectar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PruebaDatos
+{
+    class conectar
+    {
+        MySqlDataReader Lector;
+        MySqlCommand Comando;
+        MySqlConnection Conector;
+
+        public string obtenerDato() {
+            String Dato="";
+
+            Conector = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=pruebas; password=driftking125");
+            Conector.Open();
+
+            Comando = new MySqlCommand(string.Format("SELECT Resultado FROM pruebas.cambiardato WHERE id_resultado=1;"), Conector);
+
+            Lector = Comando.ExecuteReader();
+
+            while (Lector.Read())
+            {
+                Dato = Lector.GetString(0);
+            }
+            Conector.Close();
+            return Dato;
+        }
+
+        public int actualizarDato(string data) {
+
+            int resultado = -1;
+                Conector = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=pruebas; password=driftking125");
+                Conector.Open();
+
+                if (data != "NULL"){
+                    Comando = new MySqlCommand(string.Format("UPDATE pruebas.cambiardato SET Resultado='{0}' WHERE id_resultado=1;", data), Conector);
+                    resultado = 50;
+                }
+
+                Lector = Comando.ExecuteReader();
+
+                while (Lector.Read())
+                {
+                    resultado = 50;
+                }
+                Conector.Close();
+
+            return resultado;
+        }
+    }
+}
