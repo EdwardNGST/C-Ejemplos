bbb41f5ab9520306bf4516ec943f5f6abda3e01d
EdwardNGST <alan_hernandezgarcia@hotmail.com>
2015-12-14 15:48:29 -0600

    Prueba Conexion

---
+++ pruebaConexion/pruebaConexion/conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+namespace pruebaConexion
+{
+    public class conexion
+    {
+        
+
+        public string ObtenerConexion() {
+            String dato="";
+            MySqlConnection conect;
+            MySqlCommand comand;
+            MySqlDataReader dr;
+            conect = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=horario;Password=driftking125");
+            conect.Open();
+            comand = new MySqlCommand(string.Format("SELECT Materia FROM horario.semestre where Maestro='Pelao';"), conect);
+
+            dr = comand.ExecuteReader();
+            while (dr.Read())
+            {
+                dato = dr.GetString(0);
+            }
+            conect.Close();
+            return dato;
+        }
+    }
+}
